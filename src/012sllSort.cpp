e10f8d19b5f9f97968f8c712f6b4e7025187d6cc
hemuinakollu <hemuinakollu@missionrnd.com>
2016-03-09 21:51:48 +0530

    Submit Commit

--- src/012sllSort.cpp
+++ src/012sllSort.cpp
@@ -22,5 +22,51 @@ struct node {
 };
 
 void sll_012_sort(struct node *head){
+	int c1 = 0, c2 = 0, c3 = 0;
+	struct node *iterator = (struct node*)malloc(sizeof(struct node));
+	iterator = head;
+	while (iterator!=NULL)
+	{
+		if (iterator->data == 0)
+		{
+			c1++;
+		}
+		else if (iterator->data == 1)
+		{
+			c2++;
+		}
+		else if (iterator->data == 2)
+		{
+			c3++;
+		}
+		iterator = iterator->next;
+	}
+	iterator = head;
+	int i = c1+c2+c3;
+	while (i >0)
+	{
+		if (c1>0)
+		{
+			iterator->data = 0;
+			iterator = iterator->next;
+			c1--;
+		}
+		else if (c2>0)
+		{
+			iterator->data = 1;
+			iterator = iterator->next;
+			c2--;
+
+
+		}
+		else if (c3>0)
+		{
+			iterator->data = 2;
+			iterator = iterator->next;
+			c3--;
+		}
+		i--;
+	}
+	
 	
 }
